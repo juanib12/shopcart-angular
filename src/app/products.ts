ef0b7fca878c320fea6a0903a9c270a13d59bfd8
juanib12 <bjuan.ignacio11@gmail.com>
2023-03-13 16:04:46 -0300

    add image a product

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,27 +1,29 @@
 export interface Product {
-    id: number;
-    name: string;
-    price: number;
-    description: string;
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    image: 'https://picsum.photos/200/300'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras', image: 'https://picsum.photos/200/300'
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    description: '', image: 'https://picsum.photos/200/300'
   }
-  
-  export const products = [
-    {
-      id: 1,
-      name: 'Phone XL',
-      price: 799,
-      description: 'A large phone with one of the best screens'
-    },
-    {
-      id: 2,
-      name: 'Phone Mini',
-      price: 699,
-      description: 'A great phone with one of the best cameras'
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: ''
-    }
-  ]
+]
